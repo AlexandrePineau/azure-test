44038ac8f53bb421b7b249c6520e9ba556765743
Alexandre Pineau <40501622+AlexandrePineau@users.noreply.github.com>
2022-02-22 14:06:36 -0500

    Added example PDF from blob storage

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
-    echo('Hello World!')
+    echo("<h1>Hello World!</h1>");
+    echo("<a href='https://ajatcapstone.blob.core.windows.net/test/SneechDrawing.pdf'>PDF from blob storage</a>");
+
 ?>
